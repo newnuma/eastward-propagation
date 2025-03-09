4f95521cfd89473db0a6f90489d850424cf7b240
newnuma <ninuma.dy.1009nkojr@gmail.com>
2025-03-09 10:55:21 +0900

    エラー修正

--- src/analysis/analysis_for_paper/budget_functions/entrain.m
+++ src/analysis/analysis_for_paper/budget_functions/entrain.m
@@ -17,14 +17,14 @@ if (pres_index == "mld")
     [mean_data, buttom_now] = mld_mean(alldata);
     %翌月の混合層深度の水温　T(x,y,h(t+1),t)
     buttom_next = NaN(numel(slon),numel(slat),numel(time));
-    for ti=1:numel(time)
+    for ti=1:numel(time)-1
         for la=1:numel(slat)
             for lo=1:numel(slon)
                k = 1;
-               while  mldD(lo,la,ti+1) - pres(k) + (wh_.v(lo,la,ti)+wh_.v(lo,la,ti+1))/2 > 0 && k < PR
+               while  depth(lo,la,ti+1) - pres(k) + (wh_.v(lo,la,ti)+wh_.v(lo,la,ti+1))/2 > 0 && k < numel(pres)
                    k = k+1;end
                if k>1  %!!!!
-                   buttom_next(lo,la,ti)=(mldD(lo,la,ti+1)+ (wh_.v(lo,la,ti)+wh_.v(lo,la,ti+1))/2-pres(k-1))*...
+                   buttom_next(lo,la,ti)=(depth(lo,la,ti+1)+ (wh_.v(lo,la,ti)+wh_.v(lo,la,ti+1))/2-pres(k-1))*...
                        (alldata(lo,la,k,ti)-alldata(lo,la,k-1,ti))...
                        /(pres(k)-pres(k-1))+alldata(lo,la,k-1,ti);
                end
@@ -40,17 +40,17 @@ else
     buttom_data = squeeze(alldata(:,:,pres_index,:)); 
 end
 
-entrain_=NaN(141,91,TIM);
-for ti = 1:TIM-1
-    for lo = slon
-        for la = slat
+entrain1= NaN(numel(slon),numel(slat),numel(time));
+for ti = 1:numel(time)-1
+    for la=1:numel(slat)
+        for lo=1:numel(slon)
 
-            entrain_(lo,la,ti)= -(mean_data(lo,la,ti)-buttom_data(lo,la,ti))/depth(lo,la,ti)...
+            entrain1(lo,la,ti)= -(mean_data(lo,la,ti)-buttom_data(lo,la,ti))/depth(lo,la,ti)...
                 *(depth(lo,la,ti+1)-(depth(lo,la,ti)+(wh_.v(lo,la,ti+1)+wh_.v(lo,la,ti))/2));
 
             %混合層が浅くなる期間を除外
             if depth(lo,la,ti+1)-depth(lo,la,ti)+(wh_.v(lo,la,ti+1)+wh_.v(lo,la,ti))/2<=0
-                entrain_(lo,la,ti)=NaN;
+                entrain1(lo,la,ti)=NaN;
 
             end
 
@@ -58,7 +58,7 @@ for ti = 1:TIM-1
     end
 end
 
-entrain_(:,:,time(end))=NaN;
-entrain_.v = entrain_;
+entrain1(:,:,end)=NaN;
+entrain_.v = entrain1;
 entrain_ = anomaly(entrain_);
 
