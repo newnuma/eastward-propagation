f5d1e25bc48ac13b9951f36eae70331f97d8217c
newnuma <ninuma.dy.1009nkojr@gmail.com>
2025-03-25 22:50:44 +0900

    data別フォルダ①

--- src/initLoad.m
+++ src/initLoad.m
@@ -1,1 +1,5 @@
-load("base_setting.mat","slon","slat","time","pres");
+% 例: 北太平洋領域のルートパスを設定
+data_folder = 'C:\Users\ninum\Desktop\east_data\north_pacific';
+base_setting_path = fullfile(data_folder, "\base_data\base_setting.mat");
+
+load(base_setting_path,"slon","slat","time","pres");
